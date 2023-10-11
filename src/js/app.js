57f0ed673bb6889db77f1f9d582d9ca397ffff21
Lucas Leguizamon <lucaslegui1107@gmail.com>
2023-10-11 00:22:48 -0300

    seccion recomendados funcionanado

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,141 @@
-const API_KEY = '1ecc8c3efdbe45d62be8a5cc30f07bcd'; // API Key
-const GENRE_ID_HORROR = 27; // Genero de terror
-const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${GENRE_ID_HORROR}&sort_by=popularity.desc&language=es-MX`;
+const API_KEY = 'af52bc36eb86e5bf423dc0874dfdcfd9';
+const GENRE_ID_HORROR = 27;
+const GENRE_ID_SCIFI_FANTASY = 10765;
+// URLs para películas y series
+const MOVIE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${GENRE_ID_HORROR}&sort_by=popularity.desc&language=es-MX`;
+const TV_URL = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=${GENRE_ID_SCIFI_FANTASY}&sort_by=popularity.desc&language=es-MX`;
 
-fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        const movies = data.results;
-        displayMovies(movies);
-    })
-    .catch(error => {
-        console.error("Error al obtener las películas:", error);
-    });
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjUyYmMzNmViODZlNWJmNDIzZGMwODc0ZGZkY2ZkOSIsInN1YiI6IjY1MjYwNmEwZDM5OWU2MDBhZGRhNjY5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qC535LtHz7S9eADkl0MoVsTrcEhfJLY24VL9Xx-9VAI'
+    }
+};
 
-//funcion para mostrar las peliculas
+//obtener datos
+function fetchDataAndDisplay(url, displayFunction, type) {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            const items = data.results;
+            displayFunction(items, type);
+            //   console.log(data);
+        })
 
-document.getElementById('sortOrder').addEventListener('change', function() {
-    const moviesContainer = document.getElementById('recommended-movies');
-    let movies = JSON.parse(moviesContainer.getAttribute('data-movies')); // Obtenemos las peliculas almacenadas en el elemento como un atributo.
-    const order = this.value; // Obtener el orden seleccionado
+        .catch(error => {
+            console.error(`Error al obtener ${type === 'movie' ? 'las películas' : 'las series'}:`, error);
+        });
+}
 
-    // Ordenar las películas
-    movies.sort((a, b) => {
-        if(order === 'desc') {
+
+
+// Evento para ordenar películas
+document.getElementById('sortOrder').addEventListener('change', function () {
+    const container = document.getElementById('recommended-movies');
+    let items = JSON.parse(container.getAttribute('data-items'));
+    const order = this.value;
+
+    items.sort((a, b) => {
+        if (order === 'desc') {
             return b.vote_average - a.vote_average;
         } else {
             return a.vote_average - b.vote_average;
         }
     });
 
-    displayMovies(movies); // Mostrar las películas ordenadas
+    displayMovies(items, 'movie');
 });
 
+// Mostrar películas
+function displayMovies(items, type) {
+    const container = document.getElementById('recommended-movies');
+    container.setAttribute('data-items', JSON.stringify(items));
+    let htmlContent = '';
 
-function displayMovies(movies) {
-    const moviesContainer = document.getElementById('recommended-movies');
-    moviesContainer.setAttribute('data-movies', JSON.stringify(movies));
-    let moviesHTML = '';
-    
-    movies.forEach(movie => {
-        moviesHTML += `
-        <div class="card" data-movie-id="${movie.id}">
-        <div class="circle"></div>
-        <div class="circle"></div>
+    items.forEach(item => {
+        htmlContent += `
+            <div class="movie-card card" data-id="${item.id}">
+                <div class="card-inner">
+                    <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="${item.title || item.name}">
+                    <h4>${item.title || item.name}</h4>
+                    <p>Calificación:<i class="fa-solid fa-star fa-beat" style="color: #f7ef02;"></i> ${item.vote_average}/10</p>
+                </div>
+            </div>
+        `;
+    });
+
+    container.innerHTML = htmlContent;
+
+    const cards = document.querySelectorAll('.movie-card');
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            const itemId = card.getAttribute('data-id');
+            showModal(itemId, items, type);
+        });
+    });
+}
+
+// Obtener y mostrar películas
+fetchDataAndDisplay(MOVIE_URL, displayMovies, 'movie');
+
+// Mostrar series
+function displaySeries(series) {
+    const seriesContainer = document.getElementById('recommended-series');
+    let seriesHTML = '';
+
+    series.forEach(serie => {
+        seriesHTML += `
+        <div class="card" data-series-id="${serie.id}">
         <div class="card-inner">
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-          <h4>${movie.title}</h4>
-          <p>Calificación:<i class="fa-solid fa-star fa-beat" style="color: #f7ef02;"></i> ${movie.vote_average}/10</p>
+          <img src="https://image.tmdb.org/t/p/w500${serie.poster_path}" alt="${serie.name}">
+          <h4>${serie.name}</h4>
         </div>
         </div>
       `;
     });
 
-    moviesContainer.innerHTML = moviesHTML;
-    // evento al hacer clic en la tarjeta
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
+    seriesContainer.innerHTML = seriesHTML;
+    // Evento al hacer clic en la tarjeta para mostrar detalles de la serie
+    const seriesCards = document.querySelectorAll('.card');
+    seriesCards.forEach(card => {
         card.addEventListener('click', function () {
-            const movieId = card.getAttribute('data-movie-id');
-            showMovieReview(movieId, movies);
+            const seriesId = card.getAttribute('data-series-id');
+            showSeriesReview(seriesId, series);
         });
     });
 }
 
-//mostrar modal de pelicula
-function showMovieReview(movieId, movies) {
-    //  buscar pelicula por id
-    const movie = movies.find(m => m.id == movieId);
+function showSeriesReview(seriesId, series) {
+    const serie = series.find(s => s.id == seriesId);
+    
+    if (!serie) {
+        console.error(`Series with ID ${seriesId} not found.`);
+        return;
+    }
 
-    // data para el modal
-    const modalTitle = document.getElementById('movieModalLabel');
-    const modalReview = document.getElementById('movieReview');
-
-    modalTitle.textContent = movie.title;
-    modalReview.textContent = movie.overview || "No hay review disponible para esta película.";
-
-    // mostrar modal
-    const modal = new bootstrap.Modal(document.getElementById('movieModal'));
-    modal.show();
+    showModal(seriesId, series, 'series');
 }
 
 
 
+// Obtener y mostrar series
+fetchDataAndDisplay(TV_URL, displaySeries, 'tv');
+
+//mostrar modal
+function showModal(itemId, items, type) {
+    const item = items.find(i => i.id == itemId);
+    if (!item) {
+        console.error(`Item with ID ${itemId} not found.`);
+        return;
+    }
+
+    const modalTitle = document.getElementById(`${type}ModalLabel`);
+    const modalReview = document.getElementById(`${type}Review`);
+
+    modalTitle.textContent = item.title || item.name || 'Unknown Title';
+    modalReview.textContent = item.overview || `No hay review disponible para este ${type === 'movie' ? 'película' : 'serie'}.`;
+
+    const modal = new bootstrap.Modal(document.getElementById(`${type}Modal`));
+    modal.show();
+}
